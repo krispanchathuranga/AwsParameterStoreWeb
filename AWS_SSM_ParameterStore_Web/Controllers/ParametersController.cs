b6f8adfddb6e8358373fa6739f677e13e1bd9e8c
krispanchathuranga <49396653+krispanchathuranga@users.noreply.github.com>
2020-12-08 14:06:59 +0000

    Initial Commit

---
+++ AWS_SSM_ParameterStore_Web/Controllers/ParametersController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Amazon.SimpleSystemsManagement;
+using Amazon.SimpleSystemsManagement.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AWS_SSM_ParameterStore_Web.Controllers
+{
+    public class ParametersController : Controller
+    {
+        public List<Parameter> parmeters;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<PartialViewResult> GetParametersAsync(string name)
+        {
+            string query = "/";
+            if (name != "" | name != null)
+            {
+                query = query + name;
+            }
+            parmeters = new List<Parameter>();
+            var request = new GetParametersByPathRequest()
+            {
+                WithDecryption = true,
+                Recursive = true,
+                Path = query,
+                MaxResults = 10
+            };
+
+            parmeters = await GetParametersAsc(parmeters, null, request);
+
+            return PartialView("~/Views/Parameters/ParameterData.cshtml", parmeters.ToArray());
+        }
+
+
+        /**
+         * Max Results is 10 supported by AmazonSimpleSystemsManagementClient.GetParametersByPathAsync
+         * this method will recursively get all the results using the NextToken
+         */
+        public static async Task<List<Parameter>> GetParametersAsc(List<Parameter> paramData, string token, GetParametersByPathRequest req)
+        {
+            using (var client = new AmazonSimpleSystemsManagementClient())
+            {
+                req.NextToken = token;
+                var response = await client.GetParametersByPathAsync(req);
+                for (int j = 0; j < response.Parameters.Count; j++)
+                {
+                    paramData.Add(response.Parameters[j]);
+                }
+
+                if (response.NextToken != null)
+                {
+                    var res = await GetParametersAsc(paramData, response.NextToken, req);
+                }
+            }
+            return paramData;
+        }
+    }
+}
---
+++ AWS_SSM_ParameterStore_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AWS_SSM_ParameterStore_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
